5fbb03941574c16c59c3b25dcbf643fc68eb78d6
Lemiort <lemiort@gmail.com>
2017-12-11 20:06:48 +0300

    FormingDocumentationProcedure

---
+++ GidraSIM/GidraSIM.Core.Model/Procedures/FormingDocumentationProcedure.cs
@@ -0,0 +1,127 @@
+﻿using GidraSIM.Core.Model.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GidraSIM.Core.Model.Procedures
+{
+    public class FormingDocumentationProcedure:Procedure
+    {
+        public FormingDocumentationProcedure(ITokensCollector collector):base(1,1,collector)
+        {
+
+        }
+
+        public override void Update(ModelingTime modelingTime)
+        {
+            if (inputQueue.Count() > 0)
+            {
+                Random rand = new Random();
+                var token = inputQueue[0].Peek();
+
+                var worker = resources.Find(res => res is WorkerResource) as WorkerResource;
+                var comp = resources.Find(res => res is TechincalSupportResource) as TechincalSupportResource;
+
+                if (worker == null || comp == null)
+                {
+                    StringBuilder message = new StringBuilder("Моделирование электрической схемы. Ошибка: ");
+                    if (worker == null) message.Append(Environment.NewLine + "Отсутствует ресурс типа \"Исполнитель\"");
+                    if (comp == null) message.Append(Environment.NewLine + "Отсутствует ресурс типа \"Техническое обеспечение\"");
+                    throw new ArgumentNullException(message.ToString());
+                }
+
+                int resourceCount = 0;
+
+                if (token.Progress < 0.01)
+                {
+                    token.ProcessedByBlock = this;
+                    token.ProcessStartTime = modelingTime.Now;
+                    //блокируем ресурсы для него
+
+                    if (worker.TryGetResource())
+                    {
+                        resourceCount++;
+                    }
+                    else worker.ReleaseResource();
+
+                    if (comp.TryGetResource())
+                    {
+                        resourceCount++;
+                    }
+                    else comp.TryGetResource();
+                }
+                else
+                {
+                    resourceCount = 2;
+                }
+
+                double time = rand.Next(1, 30);
+
+                // Влияние ПК
+                #region PcImpact
+                double base_frequency = 1.5;//частота
+                double base_memory_proc = 2;//объем памяти процессора
+                double base_memory_video = 1;//объем памяти ведеокарты
+
+                time += (base_frequency - comp.Frequency) / 1000; //порядок влияния на время
+                time += (base_memory_proc - comp.Ram) / 10000;
+                time += (base_memory_video - comp.Vram) / 10000;
+                #endregion
+
+                // Влияние рабочего
+                #region WorkerImpact
+                switch (worker.WorkerQualification)
+                {
+                    case WorkerResource.Qualification.LeadCategory:
+                        time -= time / rand.Next(1, 4);
+                        break;
+                    case WorkerResource.Qualification.FirstCategory:
+                        time -= time / rand.Next(1, 5);//уменьшаем время, т.к. высокая категория\
+                        break;
+                    case WorkerResource.Qualification.SecondCategory:
+                        //базовое время подсчитано для второй категории
+                        break;
+                    case WorkerResource.Qualification.ThirdCategory:
+                        time += time / rand.Next(1, 5);
+                        break;
+                    case WorkerResource.Qualification.NoCategory:
+                        time += time / rand.Next(1, 4);
+                        break;
+                }
+                #endregion
+
+                //влияение методичики (необязательный ресурс)
+                #region Methodical region
+
+                var methodSupport = resources.Find(res => res is MethodolgicalSupportResource) as MethodolgicalSupportResource;
+                //если есть методичка, то время немного экономится
+                if ((methodSupport != null) && (methodSupport.TryGetResource()))
+                {
+                    time -= 0.01 * rand.NextDouble(); //от 0 до 15 минут
+                }
+                #endregion
+
+
+                if (resourceCount == 2)
+                {
+                    token.Progress += modelingTime.Delta / time;
+                }
+
+                if (token.Progress > 0.99)
+                {
+                    inputQueue[0].Dequeue();
+                    collector.Collect(token);
+
+                    outputs[0] = new Token(modelingTime.Now, token.Complexity) { Parent = this };
+
+                    // Освобождаем ресурсы
+                    worker.ReleaseResource();
+                    comp.ReleaseResource();
+                    if (methodSupport != null) methodSupport.ReleaseResource();
+                }
+            }
+        }
+    }
+}
